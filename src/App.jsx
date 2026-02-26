1cc25600edd8ab76b823a0332febc2ff983f44d1
Mark Francis <markfranksive@gmail.com>
2026-02-26 12:12:53 +0100

    fix: ask user to enter values to get BMI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+  const [result, setResult] = useState('');
+
+  const calculateBMI = () => {
+    if (!height || !weight) {
+      setResult('Please ensure you enter your weight and height');
+      return;
+    }
+
+    const heightInMeters = height / 100;
+    const bmi = (weight / (heightInMeters * heightInMeters)).toFixed(1);
+
+    let category = '';
+    if (bmi < 18.5) category = `Your BMI is ${bmi}. You are underweight`;
+    else if (bmi < 25) category = `Your BMI is ${bmi}. Your weight is normal`;
+    else if (bmi < 30) category = `Your BMI is ${bmi}. You are overweight`;
+    else category = `Your BMI is ${bmi}. Your are OBESE`;
+
+    setResult(category);
+  };
+
   return (
     <div>
       <div className='container'>
@@ -16,6 +38,8 @@ const App = () => {
           type='number'
           id='height'
           placeholder='eg. 170'
+          value={height}
+          onChange={(e) => setHeight(e.target.value)}
         />
 
         {/*weight*/}
@@ -24,14 +48,16 @@ const App = () => {
           type='number'
           id='weight'
           placeholder='eg. 60'
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}
         />
 
         {/*Btn*/}
 
-        <button>Calculate</button>
+        <button onClick={calculateBMI}>Calculate</button>
 
         {/*result*/}
-        <div className='result'></div>
+        <div className='result'> {result}</div>
       </div>
     </div>
   );
